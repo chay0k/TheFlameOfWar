95ebbcc409f2b0a7efc92324adca5b3d981974ed
Anton Trushchenko <ATrushchenko@ipg.com>
2023-01-10 11:35:15 +0200

    Added Telegram bot start functions

---
+++ TelegramBot/BotStart.cs
@@ -0,0 +1,36 @@
+﻿using System.Configuration;
+using System;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using System.Configuration;
+
+namespace TelegramBot;
+public static class BotStart
+{
+    static ITelegramBotClient bot;
+
+    public static void CreateBot(string token)
+    {
+        bot = new TelegramBotClient(token);
+    }
+    public static void Start()
+    {
+        Console.WriteLine(bot.GetMeAsync().Result.FirstName + " was started");
+
+        var cts = new CancellationTokenSource();
+        var cancellationToken = cts.Token;
+        var receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = { }, // receive all update types
+        };
+        BotEvents.Bot = bot;
+        bot.StartReceiving(
+            BotEvents.HandleUpdateAsync,
+            BotEvents.HandleErrorAsync,
+            receiverOptions,
+            cancellationToken
+        );
+        Console.ReadLine();
+    }
+}
--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace TelegramBot;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("test");
+        string token = ConfigurationManager.ConnectionStrings["Token"].ConnectionString;
+        BotStart.CreateBot(token);
+        BotStart.Start();
+    }
+}
